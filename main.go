42b5918b83f5e0c0669e0f85472b285cd21c0002
Chris Wangsanata <chriswangsanata@gmail.com>
2025-01-29 17:53:14 -0800

    multi server support

--- main.go
+++ main.go
@@ -1,19 +1,24 @@
 package main
 
 import (
+	"context"
 	"errors"
 	"fmt"
 	"io"
 	"net/http"
-	"os"
+	"net"
 )
 
+const keyServerAddr = "serverAddr"
+
 func getRoot(w http.ResponseWriter, r *http.Request) {
-	fmt.Printf("got / request\n")
+	ctx := r.Context()
+	fmt.Printf("got / request\n", ctx.Value(keyServerAddr))
 	io.WriteString(w, "This is my website!\n")
 }
 func getHello(w http.ResponseWriter, r *http.Request) {
-	fmt.Printf("got /hello request\n")
+	ctx := r.Context()
+	fmt.Printf("got /hello request\n", ctx.Value(keyServerAddr))
 	io.WriteString(w, "Hello, HTTP!\n")
 }
 
@@ -22,11 +27,44 @@ func main() {
 	mux.HandleFunc("/", getRoot)
 	mux.HandleFunc("/hello", getHello)
 
-	err := http.ListenAndServe(":3333", mux)
-	if errors.Is(err, http.ErrServerClosed) {
-		fmt.Printf("server closed\n")
-	} else {
-		fmt.Printf("server error: %v\n", err)
-		os.Exit(1)
+	ctx, cancelCtx := context.WithCancel(context.Background())
+	serverOne := &http.Server{
+		Addr:    ":3333",
+		Handler: mux,
+		BaseContext: func(listener net.Listener) context.Context {
+			ctx = context.WithValue(ctx, keyServerAddr, listener.Addr().String())
+			return ctx
+		},
 	}
+
+	serverTwo := &http.Server{
+		Addr:    ":4444",
+		Handler: mux,
+		BaseContext: func(listener net.Listener) context.Context {
+			ctx = context.WithValue(ctx, keyServerAddr, listener.Addr().String())
+			return ctx
+		},
+	}
+
+	go func() {
+		err := serverOne.ListenAndServe()
+		if errors.Is(err, http.ErrServerClosed) {
+			fmt.Printf("serverOne closed\n")
+		} else  if err != nil {
+			fmt.Printf("serverOne error: %v\n", err)
+		}
+		cancelCtx()
+	}()
+
+	go func()  {
+		err := serverTwo.ListenAndServe()
+		if errors.Is(err, http.ErrServerClosed) {
+			fmt.Printf("serverTwo closed\n")
+		} else  if err != nil {
+			fmt.Printf("serverTwo error: %v\n", err)
+		}
+		cancelCtx()
+	}()
+
+	<-ctx.Done()
 }
